8b0ed8e6c3245233f106f4f2dff942f74216d3c0
Conor Brady <Conor.Brady@freedompay.com>
2022-12-05 07:25:26 -0500

    complete day 5

---
+++ 05/main.py
@@ -0,0 +1,61 @@
+import re
+def get_input():
+    with open('input.txt') as f:
+        lines = [line for line in f.readlines()]
+    return lines
+	
+def create_stacks(lines):
+    stacks = {}
+    for i in range(1,10):
+        stacks[i] = []
+    lines.reverse()
+    for line in lines:
+        for index, i in enumerate(range(1, 34, 4)):
+            c = line[i:i+1]
+            if(c != ' ' ):
+                stacks[index+1].append(c)
+    return stacks
+
+def parse_instruction(line):
+    pattern = r'\d+'
+    m = re.findall(pattern, line)
+    return int(m[0]), int(m[1]), int(m[2])
+    
+    
+def main():
+    all_lines = get_input()
+    init_stacks = all_lines[:8]
+    stacks = create_stacks(init_stacks)
+    stacks2 = {key: value[:] for key, value in stacks.items()}
+    
+    instructions = all_lines[10:]
+    
+    # instructions = instructions[:5]
+    # print(instructions)
+    
+    # Part 1 - Crane Mover 9000
+    for instr in instructions:
+        amount, source, dest = parse_instruction(instr)
+        for i in range(0, amount):
+            temp = stacks[source].pop()
+            stacks[dest].append(temp)
+            
+    # print(stacks)
+    
+    for k,v in stacks.items():
+        print(v[-1], sep='', end='')
+        
+    print('\n----')
+    # Part 2 - Crane Mover 9000
+    stacks = stacks2
+
+    for instr in instructions:
+        amount, source, dest = parse_instruction(instr)
+        stacks[dest] += stacks[source][-amount:]
+        del stacks[source][-amount:]
+    
+    for k,v in stacks.items():
+        print(v[-1], sep='', end='')
+    
+if __name__ == '__main__':
+	main()
